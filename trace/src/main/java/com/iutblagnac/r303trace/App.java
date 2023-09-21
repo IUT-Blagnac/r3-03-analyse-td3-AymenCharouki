107fbe2442a81a8afd676037285ebc53a94b5803
Aymen Charouki <charoukiaymen@gmail.com>
2023-09-21 12:23:30 +0200

    FIN DU TP3

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -6,15 +6,37 @@ package com.iutblagnac.r303trace;
  */
 public class App 
 {
+	
+	/**
+	 * La fonction main qui appelle les méthodes de App
+	 * 
+	 * @param args
+	 * 
+	 */
     public static void main( String[] args )
     {
         System.out.println(hello());
+        System.out.println(hello("Aymen Charouki TP3"));
     }
+    
+    /**
+     * La version non paramètré de la méthode hello()
+     * 
+     * @return String contenant toujours la chaine de charactères : Hello World!
+     * 
+     */
 
     public static String hello() {
         return  "Hello World!" ;
     }
 
+    /**
+     * La version paramètré de la méthode hello()
+     * 
+     * @param param la chaine de caractères qu'on veut affiché
+     * 
+     * @return la chaine de caractère passé en paramètres
+     */
     public static String hello(String param) {
         return  param ;
     }
